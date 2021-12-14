7748e3b668a12512d6e083a3c3093ca4a1aae1e1
dmitruk <dmitruk_89@mail.ru>
2021-12-14 18:45:29 +0300

    solution

---
+++ index.js
@@ -0,0 +1,48 @@
+let str = "Привет"
+console.log(typeof str)
+let number = 123
+console.log(typeof number)
+let flag = true
+console.log(typeof flag)
+let txt = "true"
+console.log(typeof txt)
+
+
+let a = 5
+let b = 3
+let а1 = a + b
+console.log(а1)
+let a2 = a - b
+console.log(a2)
+let a3 = a * b
+console.log(a3)
+let a4 = a/b
+console.log(a4)
+
+
+let a5 = a % b
+console.log(a5)
+let a6 = b%a
+console.log(a6)
+let a7 = a+"b"
+console.log(a7)
+let a8 = "a" - b
+console.log(a8)
+let a9 = 75 + 'кг'
+console.log(a9)
+
+let w = 23
+let h = 10
+let s = w * h + "см"
+console.log(s)
+
+let heightC = 10
+let dC = 4
+let pi = 3.14
+let v = pi * Math.pow(dC,2) / 4 * heightC + ' м.куб'
+console.log(v)
+
+let n = 3
+let m = 4
+let k = n**n + m**m
+console.log(k)
